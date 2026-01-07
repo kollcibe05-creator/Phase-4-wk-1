9b74c932e458a9a745a2ddd77318db079f364a2f
Collins Kibet <kollcibe05@gmail.com>
2026-01-08 00:12:46 +0300

    basic models importations

---
+++ server/models.py
@@ -0,0 +1,13 @@
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import MetaData
+from sqlalchemy_serializer import SerializerMixin
+
+metadata = MetaData(naming_convention={
+    "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
+})
+
+db = SQLAlchemy(metadata=metadata)
+
+
+
+###models ~ must import serializermixin, have serialization-proxy, serialize_rules
