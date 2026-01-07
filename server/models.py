7aef3c13a8826fdc0ecb94c10e5907c7c06aa72f
Collins Kibet <kollcibe05@gmail.com>
2026-01-08 00:32:38 +0300

    created the three models

--- server/models.py
+++ server/models.py
@@ -10,4 +10,36 @@ db = SQLAlchemy(metadata=metadata)
 
 
 
-###models ~ must import serializermixin, have serialization-proxy, serialize_rules
+###models ~ must import serializermixin, have serialization-proxy, serialize_rules, cascades
+
+class Hero(db.Model, SerializerMixin):
+    __tablename__ = "heroes"
+
+    serialize_rules = ()
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    super_name = db.Column(db.String)
+
+class Power(db.Model, SerializerMixin):
+    __tablename__ = "powers"
+
+    serialize_rules = ()
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    description = db.Column(db.String)
+
+class HeroPower(db.model, SerializerMixin):
+    __tablename__ == "hero_powers"
+
+    serialize_rules = ()
+
+    id = db.Column(db.Integer, primary_key=True)
+    strength = db.Column(db.String)
+    hero_id = db.Column(db.Integer, db.ForeinKey("heroes.id"))
+    power_id = db.Column(db.Integer, db.ForeinKey("powers.id"))
+    
+
+
+
